3bcb3f5ae8533139279845cb009a07a915c32864
Bryan Hunt <bhunt@mac.com>
2023-03-10 23:40:37 -0600

    Get sniffer working

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,29 @@
 import { SerialPort } from 'serialport';
+import { ByteLengthParser } from '@serialport/parser-byte-length';
+import { toHexWords } from '@springfield/ham-radio-driver-utils';
 
 try {
-  const port = new SerialPort({ path: process.argv[2], baudRate: 9600 });
-  port.write([0x06]);
-  port.drain(() => port.close());
+  const computerPort = new SerialPort({ path: process.argv[2], baudRate: 9600 });
+  const computerParser = computerPort.pipe(new ByteLengthParser({ length: 1 }));
+
+  const radioPort = new SerialPort({ path: process.argv[3], baudRate: 9600 });
+  const radioParser = radioPort.pipe(new ByteLengthParser({ length: 1 }));
+  let radioData: number[] = [];
+
+  computerParser.on('data', (data) => {
+    radioPort.write(data);
+
+    if (radioData.length > 1) {
+      console.log(toHexWords(Uint8Array.from(radioData)));
+    }
+
+    radioData = [];
+  });
+
+  radioParser.on('data', (data) => {
+    computerPort.write(data);
+    radioData.push(data[0]);
+  });
 } catch (error) {
   console.log(error);
 }
