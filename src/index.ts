3f4dd573c353b852a31aa1c416935dc0abf3f759
Bryan Hunt <bhunt@nvidia.com>
2025-06-01 10:45:59 -0500

    Add logging for computer side and update tsconfig

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { SerialPort } from 'serialport';
 import { ByteLengthParser } from '@serialport/parser-byte-length';
-import { toHexWords } from '@springfield/ham-radio-driver-utils';
+import { toHexWords } from '@springfield/ham-radio-utils';
 
 try {
   const computerPort = new SerialPort({ path: process.argv[2], baudRate: 9600 });
@@ -8,21 +8,27 @@ try {
 
   const radioPort = new SerialPort({ path: process.argv[3], baudRate: 9600 });
   const radioParser = radioPort.pipe(new ByteLengthParser({ length: 1 }));
-  let radioData: number[] = [];
+  let dataFromComputer: number[] = [];
+  let dataFromRadio: number[] = [];
 
   computerParser.on('data', (data) => {
     radioPort.write(data);
+    dataFromComputer.push(data[0]);
 
-    if (radioData.length > 1) {
-      console.log(toHexWords(Uint8Array.from(radioData)));
+    if (dataFromRadio.length > 1) {
+      console.log(`fromRadio: ${toHexWords(Uint8Array.from(dataFromRadio))}`);
+      dataFromRadio = [];
     }
-
-    radioData = [];
   });
 
   radioParser.on('data', (data) => {
     computerPort.write(data);
-    radioData.push(data[0]);
+    dataFromRadio.push(data[0]);
+
+    if (dataFromComputer.length > 1) {
+      console.log(`fromComputer: ${toHexWords(Uint8Array.from(dataFromComputer))}`);
+      dataFromComputer = [];
+    }
   });
 } catch (error) {
   console.log(error);
